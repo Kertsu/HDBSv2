94cbf94f288147b02da75b4ea4f9bf2bb81b761a
Kertsu <kurtddbigtas@gmail.com>
2024-01-28 02:00:50 +0800

    try again ws

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,11 @@ const app = express();
 const port = process.env.PORT || 5000
 const {urlencoded} = require('body-parser');
 const connectDB = require("./config/db");
+// const cors = require('cors');
 
-const http = require('http');
 const WebSocket = require('ws')
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
+const wss = new WebSocket.Server({port: 3000});
 
 wss.on('connection', (ws) => {
     ws.on('message', (message)=> {
@@ -20,6 +19,22 @@ wss.on('connection', (ws) => {
 
 
 connectDB();
+
+// const allowedOrigins = ['http://localhost:4200', 'http://localhost:8000', 'https://hdbsv2.onrender.com'];
+
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     if (allowedOrigins.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+  
+// }));
+
+// app.set('trust proxy', true)
+
 
 server.listen(3000, () => {
     console.log('Server started on port 3000');
