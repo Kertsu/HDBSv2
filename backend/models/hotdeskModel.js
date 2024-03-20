60238fc844e238fe4cb3c141115210d583c7992e
Kertsu <kurtddbigtas@gmail.com>
2024-03-20 15:59:59 +0800

    added hotdesk model

---
+++ backend/models/hotdeskModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const hotdeskSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique:true
+  },
+  deskNumber: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  workspaceEssentials: {
+    type: [String],
+    enum: [
+      "Desk Organizer",
+      "Noise-Canceling Headphones",
+      "Desk Plants",
+      "Personalized Nameplate",
+      "Cubicle Mirror",
+      "Footrest",
+      "Desk Lamp",
+      "Bulletin Board",
+      "Mini Fridge",
+      "Task Lighting",
+      "Whiteboard",
+      "Under-Desk Storage",
+      "Cubicle Shelf",
+      "Cubicle Privacy Screen",
+      "Ergonomic Chair Cushion",
+    ],
+  },
+  status:{
+    type: String, 
+    enum:["AVAILABLE","UNAVAILABLE"],
+    default: "AVAILABLE"
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Hotdesk', hotdeskSchema);
