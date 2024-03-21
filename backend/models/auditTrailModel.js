28f831eb292a55117d818f53df60402bc1c120f4
Kertsu <kurtddbigtas@gmail.com>
2024-03-22 02:08:28 +0800

    added audit trail files

---
+++ backend/models/auditTrailModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const auditTrailSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }, 
+    name: {
+        type: String,
+    },
+    email:{
+        type: String,
+    },
+    actionType:{
+        type: String,
+        required: true
+    },
+    actionDetails:{
+        type: String,
+        required: true
+    },
+    ipAddress:{
+        type: String,
+        required: true
+    },
+    status:{
+        type: String,
+        required: true
+    },
+    additionalContext:{
+        type: String
+    },
+
+}, {timestamps: true})
+
+module.exports = mongoose.model('AuditTrail', auditTrailSchema)
