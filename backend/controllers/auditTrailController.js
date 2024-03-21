63c4166c324e9e42d892b39d5b7f6654d7fa2c68
Kertsu <kurtddbigtas@gmail.com>
2024-03-22 02:11:02 +0800

    added get trails

--- backend/controllers/auditTrailController.js
+++ backend/controllers/auditTrailController.js
@@ -1,43 +1,49 @@
 const asyncHandler = require("express-async-handler");
 const AuditTrail = require("../models/auditTrailModel");
+const queryHelper = require("../utils/queryHelper");
 
 const getAuditTrails = asyncHandler(async (req, res) => {
- 
+ const trails = await queryHelper(AuditTrail, req.query, 'trail')
+
+ return res.status(200).json({
+  success: true, 
+  trails
+ })
 });
 
-const createAuditTrail = asyncHandler(async (req, res) => {
-  const ipAddress =
-    req.ip ||
-    req.headers["x-forwarded-for"] ||
-    req.headers["true-client-ip"] ||
-    req.remoteAddress ||
-    req.socket.remoteAddress ||
-    null;
-  const userId = req.user?._id;
+// const createAuditTrail = asyncHandler(async (req, res) => {
+//   const ipAddress =
+//     req.ip ||
+//     req.headers["x-forwarded-for"] ||
+//     req.headers["true-client-ip"] ||
+//     req.remoteAddress ||
+//     req.socket.remoteAddress ||
+//     null;
+//   const userId = req.user?._id;
   
 
-  const { email, actionType, actionDetails, status, additionalContext } =
-    req.body;
+//   const { email, actionType, actionDetails, status, additionalContext } =
+//     req.body;
 
-  const auditTrail = await AuditTrail.create({
-    user: userId ?? null,
-    email: req.user?.email ?? email,
-    actionType,
-    actionDetails,
-    status,
-    additionalContext,
-    ipAddress,
-  });
+//   const auditTrail = await AuditTrail.create({
+//     user: userId ?? null,
+//     email: req.user?.email ?? email,
+//     actionType,
+//     actionDetails,
+//     status,
+//     additionalContext,
+//     ipAddress,
+//   });
 
-  // io.emit('new-trail', auditTrail)
+//   // io.emit('new-trail', auditTrail)
 
-  // console.log(io.emit('new-trail', auditTrail))
+//   // console.log(io.emit('new-trail', auditTrail))
 
-  console.log('exec')
-  res.status(201).json({
-    message: 'success',
-    auditTrail
-  })
-});
+//   console.log('exec')
+//   res.status(201).json({
+//     message: 'success',
+//     auditTrail
+//   })
+// });
 
 module.exports = { createAuditTrail, getAuditTrails };
