691345aa6b9869cb99d747856d6edfcc78eac9b3
K3rtsu <--global>
2024-04-15 22:40:05 +0800

    add hashPassword helper

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -6,3 +6,8 @@ const isValidEmail = (email) => {
   const emailRegex = /@(student\.laverdad\.edu\.ph|laverdad\.edu\.ph)$/i;
   return email.match(emailRegex);
 };
+
+const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+  };
