bf98f54afd2ebe55c9f2347ba5a9951d0cc48023
Kertsu <kurtddbigtas@gmail.com>
2024-05-26 17:59:16 +0800

    integrate audit trail: createFeedback

--- backend/controllers/feedbackController.js
+++ backend/controllers/feedbackController.js
@@ -1,27 +1,55 @@
 const asyncHandler = require("express-async-handler");
 const Feedback = require("../models/feedbackModel");
-const Hotdesk = require('../models/hotdeskModel');
+const Hotdesk = require("../models/hotdeskModel");
 const queryHelper = require("../utils/queryHelper");
+const ActionType = require("../utils/trails.enum");
+const { createAuditTrail } = require("../utils/helpers");
 
 const createFeedback = asyncHandler(async (req, res) => {
   const { deskNumber, rating, description } = req.body;
 
+  const actionType = ActionType.FEEDBACK;
+  const actionDetails = `submit feedback for Hotdesk #${deskNumber}`;
+  let error;
+
   if (!deskNumber || !rating) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Missing required fields" });
+    error = "Missing required fields";
+    createAuditTrail(req, {
+      actionType,
+      actionDetails,
+      status: "failed",
+      additionalContext: error,
+    });
+    console.log(actionDetails, "det");
+    console.log(actionType, "type");
+    return res.status(400).json({ success: false, error });
   }
 
-  const desk = await Hotdesk.findOne({ deskNumber });
+  try {
+    const desk = await Hotdesk.findOne({ deskNumber });
+    if (!desk) {
+      throw new Error("Desk not found");
+    }
 
-  const feedback = await Feedback.create({
-    desk: desk.id || desk._id,
-    rating,
-    description: description || null,
-    user: req.user._id,
-  });
+    const user = req.user;
 
-  return res.status(200).json({ success: true, feedback });
+    const feedback = await Feedback.create({
+      desk: desk.id || desk._id,
+      rating,
+      description: description || null,
+      user: user._id,
+    });
+
+    createAuditTrail(req, {
+      actionType,
+      actionDetails,
+      status: "success",
+      additionalContext: `${user.username} gave a ${rating}/5 rating on Hotdesk #${deskNumber}`,
+    });
+    return res.status(200).json({ success: true, feedback });
+  } catch (error) {
+    return res.status(400).json({ success: false, error: error.message });
+  }
 });
 
 const getFeedbacks = asyncHandler(async (req, res) => {
